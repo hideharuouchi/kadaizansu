3b05ca5b91d013a16a538e6cdd59fa489a4c876c
Hideharu OUCHI <a1705ho@aiit.ac.jp>
2017-06-08 23:27:55 +0900

    travis CI test

--- test_kadai2.sh
+++ test_kadai2.sh
@@ -2,14 +2,14 @@
 
 
 ERROR_EXIT (){
-	echo $1 1>&2
-	exit 1
+  echo $1 1>&2
+  exit 1
 }
 
 # TEST1-1: output check
 echo "ans = 4" > /tmp/$$-ans
 ./kadai2.sh 4 12 > /tmp/$$-out || ERROR_EXIT "exe chigau"
-diff /tmp/$$-ans /tmp$$-out || ERROR_EXIT "diff chigau"
+diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
 
 # TEST1-2
 echo "ans = 21" > /tmp/$$-ans
