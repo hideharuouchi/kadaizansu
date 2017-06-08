7b51440c8ab6d38e8e6a32095241d97c163cf240
Hideharu OUCHI <a1705ho@aiit.ac.jp>
2017-06-08 23:59:15 +0900

    travis CI test

--- test_kadai2.sh
+++ test_kadai2.sh
@@ -16,4 +16,15 @@ echo "ans = 21" > /tmp/$$-ans
 ./kadai2.sh 42 21 > /tmp/$$-out || ERROR_EXIT "exe chigau"
 diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
 
+# TEST1-3
+echo "ans = 7" > /tmp/$$-ans
+./kadai2.sh 14 21 > /tmp/$$-out || ERROR_EXIT "exe chigau"
+diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
+
+# TEST1-4
+echo "ans = 13" > /tmp/$$-ans
+./kadai2.sh 52 39 > /tmp/$$-out || ERROR_EXIT "exe chigau"
+diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
+
+
 echo OK
