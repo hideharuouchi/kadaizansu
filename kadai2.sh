bff2a89277dac8c6cb44f3211cd24cada792de9c
Hideharu OUCHI <a1705ho@aiit.ac.jp>
2017-06-08 22:11:33 +0900

    travis CI test

---
+++ kadai2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+a=$1
+b=$2
+
+if [ $a -lt $b ]; then
+	temp=$b
+	b=$a
+	a=$temp
+fi
+
+r=`expr $a % $b`
+
+while [ $r != "0" ]
+do
+	a=$b
+	b=$r
+	r=`expr $a % $b`
+done
+
+echo "The greatest common divisor: $b"
---
+++ test_kadai2.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+ERROR_EXIT (){
+	echo $1 1>&2
+	exit 1
+}
+
+# TEST1-1: output check
+echo "ans = 4" > /tmp/$$-ans
+./kadai2.sh 4 12 > /tmp/$$-out || ERROR_EXIT "exe chigau"
+diff /tmp/$$-ans /tmp$$-out || ERROR_EXIT "diff chigau"
+
+# TEST1-2
+echo "ans = 21" > /tmp/$$-ans
+./kadai2.sh 42 21 > /tmp/$$-out || ERROR_EXIT "exe chigau"
+diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
+
+echo OK
